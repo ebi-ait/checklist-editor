e56c9eecc5fd0f23777a5b096d8afa50a0617cf1
Amnon Khen <amnon@ebi.ac.uk>
2024-07-23 12:37:15 +0100

    ck-104 use schema store paging api

--- src/schemaStoreDataProvider.tsx
+++ src/schemaStoreDataProvider.tsx
@@ -1,7 +1,21 @@
-import { DataProvider, fetchUtils } from 'react-admin';
+import {DataProvider, fetchUtils, GetListResult} from 'react-admin';
 
 const apiUrl = '/api/biosamples/schema/store/api/v2/';
 const httpClient = fetchUtils.fetchJson;
+
+const resourceMap = {
+    checklists: 'schemas',
+};
+
+function throwNotImplmentedError(resource: string, functionName: string) {
+    const errMsg = `${functionName}(${resource}) not implemented`;
+    console.error(errMsg);
+    throw new Error(errMsg)
+}
+
+function filterBSDChecklists(data) {
+    return data.filter(checklist => !checklist.accession.startsWith('BSD'))
+}
 
 const schemaStoreDataProvider: DataProvider = {
     getList: (resource, params) => {
@@ -9,86 +23,50 @@ const schemaStoreDataProvider: DataProvider = {
 
         const query = new URLSearchParams({
             ...filter.q ? { text: filter.q } : {}, // Add the 'text' parameter if 'q' is provided
-            start: pagination.page * pagination.perPage - pagination.perPage,
-            limit: pagination.perPage,
+            number: pagination.page-1, // react-admin is 1 based, spring is 0 based
+            size: pagination.perPage,
             sort: sort.field,
             order: sort.order,
         }).toString();
-
         // Adjust the URL to point to the right endpoint for lists
-        const url = `${apiUrl}${resource}/search?${query}`;
+        const apiResource = resourceMap[resource];
+        const url = `${apiUrl}${apiResource}/search?${query}`;
         return httpClient(url).then(({ json }) => {
             // Extract the embedded resources
-            const data = json._embedded?.[resource] || [];
+            let data = json._embedded?.[apiResource] || [];
             return {
                 data,
                 total: json.page?.totalElements || data.length,
-            };
+                pageInfo: {
+                    hasNextPage: json?._links?.next || false,
+                    hasPreviousPage: json?._links?.prev || false
+                }
+            } ;
         });
     },
     getOne: (resource, params) => {
-        return httpClient(`${apiUrl}${resource}?id=${params.id}`).then(({ json }) => {
+        const apiResource = resourceMap[resource];
+        return httpClient(`${apiUrl}${apiResource}?id=${params.id}`).then(({ json }) => {
             const data = json; // Assuming json is the schema object itself
             return { data };
         });
     },
     getMany: (resource, params) => {
-        return httpClient(`${apiUrl}${resource}/search?id=${params.ids.join(',')}`).then(({ json }) => {
-            const data = json._embedded?.[resource] || [];
-            return { data };
-        });
+        throwNotImplmentedError(resource, 'getMany');
     },
     getManyReference: (resource, params) => {
-        const url = `${apiUrl}${resource}/search?${params.target}=${params.id}`;
-        return httpClient(url).then(({ json }) => {
-            const data = json._embedded?.[resource] || [];
-            return {
-                data,
-                total: json.page?.totalElements || data.length,
-            };
-        });
+        throwNotImplmentedError(resource, 'getManyReference');
     },
     create: (resource, params) => {
-        return httpClient(`${apiUrl}${resource}`, {
-            method: 'POST',
-            body: JSON.stringify(params.data),
-        }).then(({ json }) => ({
-            data: { ...params.data, id: json.id },
-        }));
+        throwNotImplmentedError(resource, 'create');
     },
     update: (resource, params) => {
-        const url = `${apiUrl}${resource}?id=${params.id}`;
-        return httpClient(url, {
-            method: 'PUT',
-            body: JSON.stringify(params.data),
-        }).then(({ json }) => ({
-            data: json,
-        }));
+        throwNotImplmentedError(resource, 'update');
     },
     updateMany: (resource, params) => {
-        return httpClient(`${apiUrl}${resource}`, {
-            method: 'PUT',
-            body: JSON.stringify(params.data),
-        }).then(({ json }) => ({
-            data: json,
-        }));
-    },
-    delete: (resource, params) => {
-        const url = `${apiUrl}${resource}?id=${params.id}`;
-        return httpClient(url, {
-            method: 'DELETE',
-        }).then(({ json }) => ({
-            data: json,
-        }));
     },
     deleteMany: (resource, params) => {
-        const url = `${apiUrl}${resource}?id=${params.ids.join(',')}`;
-        return httpClient(url, {
-            method: 'DELETE',
-        }).then(({ json }) => ({
-            data: json,
-        }));
+        throwNotImplmentedError(resource, 'deleteMany');
     },
 };
-
 export default schemaStoreDataProvider;
