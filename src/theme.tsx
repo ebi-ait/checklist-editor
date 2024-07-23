543305052498b449335a58e790cc4c70e7c90f0c
Amnon Khen <amnon@ebi.ac.uk>
2024-07-23 12:46:37 +0100

    ck-104 ebi theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,13 +1,12 @@
-import { defaultTheme } from 'react-admin';
+import { defaultTheme, nanoLightTheme } from 'react-admin';
 // EBI visual styles taken from https://stable.visual-framework.dev/
 export const appTheme = {
     ...defaultTheme,
     palette: {
     //     mode: 'light',
-        primary: {main: '#18974c'},
-        secondary: {main: '#0a5032'},
+        primary: {main: '#6cc24a'},
+        secondary: {main: '#007b53'},
         error: {main: '#d32f2f'},
-        background: {main: '#fffadc'},
     },
     typography: {
         // Use the system font instead of the default Roboto font.
