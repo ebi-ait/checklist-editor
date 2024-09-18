a0c13221cbc7684de440eb18285c5a83d04812ad
Amnon Khen <amnon@ebi.ac.uk>
2024-09-18 22:13:13 +0100

    ck-61 refactor checklist form

--- src/components/ChecklistCreate.tsx
+++ src/components/ChecklistCreate.tsx
@@ -1,28 +1,8 @@
-import {ArrayInput, Create, ReferenceInput, SelectInput, SimpleForm, SimpleFormIterator, TextInput} from "react-admin";
+import {Create} from "react-admin";
+import {ChecklistForm} from "./ChecklistEdit.tsx";
 
 export const ChecklistCreate = props => (
     <Create {...props}>
-        <SimpleForm>
-            <TextInput source="title"/>
-            <TextInput source="description" multiline={true}/>
-            <ArrayInput source="schemaFieldAssociations" label="Fields">
-                <SimpleFormIterator inline>
-                    <SelectInput source="cardinality"
-                                 choices={[
-                                     {id: 'OPTIONAL', name: 'Optional'},
-                                     {id: 'MANDATORY', name: 'Mandatory'},
-                                     {id: 'RECOMMENDED', name: 'Recommended'},
-                                 ]}
-                                 defaultValue={'MANDATORY'}/>
-                    <SelectInput source="multiplicity"
-                                 choices={[
-                                     {id: 'Single', name: 'Single'},
-                                     {id: 'List', name: 'List'},
-                                 ]}
-                                 defaultValue={'Single'}/>
-                    <ReferenceInput source="fieldId" reference="fields"/>
-                </SimpleFormIterator>
-            </ArrayInput>
-        </SimpleForm>
+        <ChecklistForm/>
     </Create>
 );
