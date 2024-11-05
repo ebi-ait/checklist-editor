6182ba0ead7a748a7c601fc0568d3f3a4e4f5494
Amnon Khen <amnon@ebi.ac.uk>
2024-11-05 10:49:13 +0100

    ck-162 auto complete for ontology field

--- public/config.js
+++ public/config.js
@@ -3,5 +3,6 @@ window.__CONFIG__ = {
     // to /api to the biosamples server
     // for docker based deployments /api
     SCHEMA_STORE_URL: "/api/v2",
+    OLS_URL: "https://www.ebi.ac.uk/ols4/api",
     CHECKLIST_EDITOR_BASE_URL: "/biosamples/checklist-editor"
 };
