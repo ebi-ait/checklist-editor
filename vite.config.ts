5dac28fe81a250c8d242adda6a7bef0d7497743e
Amnon Khen <amnon@ebi.ac.uk>
2024-12-02 00:32:36 +0000

    ck-92 authentication

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
+function logResponse(proxyRes) {
+    return console.log(`Response received from target with status: ${proxyRes.statusCode}`);
+}
+
+function logRequest(proxyReq, req)  {
+        console.log(`Proxying request to: ${proxyReq.getHeader('host')}${proxyReq.path}`);
+        console.log(`Original URL: ${req.url}`);
+        console.log(`Proxying request with Content-Type: ${proxyReq.getHeader('Content-Type')}`);
+}
+
 // see https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
     // Load env file based on `mode` in the current working directory.
@@ -21,30 +31,23 @@ export default defineConfig(({command, mode}) => {
         ],
         server: {
             proxy: {
-                // Proxy requests to the API server
+                '/auth': {
+                    target: 'https://www.ebi.ac.uk/ena/submit/webin',
+                    changeOrigin: true,
+                    secure: false,
+                    configure: (proxy) => {
+                        proxy.on('proxyReq', logRequest);
+                        proxy.on('proxyRes', logResponse);
+                    }
+                },
+
                 '/api/v2': {
                     target: schemaStoreHost,
                     changeOrigin: true,
                     secure: false,
-                },
-
-                '/auth': {
-                    target: 'https://wwwdev.ebi.ac.uk/ena/submit/webin',
-                    changeOrigin: true,
-                    secure: false,
                     configure: (proxy) => {
-                        // Intercept the proxy request to log details
-                        proxy.on('proxyReq', (proxyReq, req) => {
-                            console.log(`Proxying request to: ${proxyReq.getHeader('host')}${proxyReq.path}`);
-                            console.log(`Original URL: ${req.url}`);
-                            if (!proxyReq.getHeader('Content-Type')) {
-                                proxyReq.setHeader('Content-Type', 'application/json'); // Set default Content-Type
-                            }
-                            console.log(`Proxying request with Content-Type: ${proxyReq.getHeader('Content-Type')}`);
-                        });
-                        proxy.on('proxyRes', (proxyRes) => {
-                            console.log(`Response received from target with status: ${proxyRes.statusCode}`);
-                        });
+                        proxy.on('proxyReq', logRequest);
+                        proxy.on('proxyRes', logResponse);
                     }
                 }
             },
